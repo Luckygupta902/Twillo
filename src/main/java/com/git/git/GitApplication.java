cc64709e0762ddff3f1e2ca62b1f49ac21ac64e1
lucky2301196 <lucky2301196@gmail.com>
2025-03-30 17:14:23 +0530

    conflit main

--- src/main/java/com/git/git/GitApplication.java
+++ src/main/java/com/git/git/GitApplication.java
@@ -9,6 +9,7 @@ public class GitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitApplication.class, args);
 		System.out.print("git merge");
+		System.out.print("git cnflit main");
 		
 	}
 
